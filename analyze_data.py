e809a73eb70d083d6a117d76b7f00764da7516ae
Yang Liu <foamliu@yeah.net>
2018-10-11 20:33:11 +0800

    update

--- analyze_data.py
+++ analyze_data.py
@@ -15,16 +15,21 @@ def analyze():
     for line in lines:
         item = json.loads(line)
         passage = item['passage']
-        passage_lengths.append(len(list(jieba.cut(passage.strip()))))
+        seg_list = jieba.cut(passage.strip())
+        length = len(list(seg_list))
+        if length <= 500:
+            passage_lengths.append(length)
         query = item['query']
-        query_lengths.append(len(list(jieba.cut(query.strip()))))
+        seg_list = jieba.cut(query.strip())
+        length = len(list(seg_list))
+        query_lengths.append(length)
 
-    num_bins = 100
+    num_bins = 10
     n, bins, patches = plt.hist(passage_lengths, num_bins, facecolor='blue', alpha=0.5)
     plt.title('Passage Lengths Distribution')
     plt.show()
 
-    num_bins = 100
+    num_bins = 10
     n, bins, patches = plt.hist(query_lengths, num_bins, facecolor='blue', alpha=0.5)
     plt.title('Query Lengths Distribution')
     plt.show()
