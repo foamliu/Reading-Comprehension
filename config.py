1824b81505347c4c58b3ff90e7410db130eec497
Yang Liu <liuyang12@focusmedia.cn>
2019-02-18 17:59:01 +0800

    update

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import json
-import os
-
 import torch
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -13,23 +11,11 @@ n_iteration = 4000
 print_every = 100
 save_every = 1
 workers = 1
-max_len = 10  # Maximum sentence length to consider
-min_word_freq = 20  # Minimum word count threshold for trimming
+min_word_freq = 10  # Minimum word count threshold for trimming
 save_dir = 'models'
-input_lang_vocab_size = 5000
-output_lang_vocab_size = 5000
 
 # Configure models
-model_name = 'cb_model'
-attn_model = 'general'
-start_epoch = 0
-epochs = 120
-hidden_size = 500
-encoder_n_layers = 2
-decoder_n_layers = 2
-dropout = 0.05
-batch_size = 100
-train_split = 0.9
+
 
 train_folder = 'data/ai_challenger_oqmrc_trainingset_20180816'
 valid_folder = 'data/ai_challenger_oqmrc_validationset_20180816'
@@ -41,23 +27,11 @@ test_a_filename = 'ai_challenger_oqmrc_testa.json'
 # num_train_samples = 8206380
 # num_valid_samples = 7034
 
-# Default word tokens
-PAD_token = 0  # Used for padding short sentences
-SOS_token = 1  # Start-of-sentence token
-EOS_token = 2  # End-of-sentence token
-UNK_token = 3
 
-start_word = '<start>'
-stop_word = '<end>'
-unknown_word = '<unk>'
-
-
-class Lang:
-    def __init__(self, filename):
-        word_map = json.load(open(filename, 'r'))
-        self.word2index = word_map
-        self.index2word = {v: k for k, v in word_map.items()}
-        self.n_words = len(word_map)
+start_word = '<START>'
+stop_word = '<END>'
+unknown_word = '<UNK>'
+pad_word = '<PAD>'
 
 
 
--- pre_process.py
+++ pre_process.py
@@ -1,40 +1,78 @@
+import json
+import os
+import pickle
+
 import jieba
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-from config import *
+from config import train_folder, train_filename, min_word_freq
 
 
-def analyze():
-    filename = os.path.join(train_folder, train_filename)
-    print(filename)
-    with open(filename, 'r', encoding='utf-8') as file:
+def seg_line(line):
+    return list(jieba.cut(line))
+
+
+def seg_data(path):
+    print('start process ', path)
+    with open(path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
-    passage_lengths = []
-    query_lengths = []
+    data = []
 
-    for line in lines:
+    for line in tqdm(lines):
         item = json.loads(line)
-        passage = item['passage']
-        seg_list = jieba.cut(passage.strip())
-        length = len(list(seg_list))
-        if length <= 500:
-            passage_lengths.append(length)
-        query = item['query']
-        seg_list = jieba.cut(query.strip())
-        length = len(list(seg_list))
-        query_lengths.append(length)
+        question = item['query']
+        doc = item['passage']
+        alternatives = item['alternatives']
+        data.append([seg_line(question), seg_line(doc), alternatives.split('|'), item['query_id']])
+    return data
 
-    num_bins = 100
-    n, bins, patches = plt.hist(passage_lengths, num_bins, facecolor='blue', alpha=0.5)
-    plt.title('Passage Lengths Distribution')
-    plt.show()
 
-    num_bins = 100
-    n, bins, patches = plt.hist(query_lengths, num_bins, facecolor='blue', alpha=0.5)
-    plt.title('Query Lengths Distribution')
-    plt.show()
+def build_word_count(data):
+    wordCount = {}
+
+    def add_count(lst):
+        for word in lst:
+            if word not in wordCount:
+                wordCount[word] = 0
+            wordCount[word] += 1
+
+    for one in data:
+        [add_count(x) for x in one[0:3]]
+    print('vocab size ', len(wordCount))
+    return wordCount
+
+
+def build_vocab(wordCount, threshold):
+    vocab = {'<PAD>': 0, '<UNK>': 1}
+    for word in wordCount:
+        if wordCount[word] >= threshold:
+            if word not in vocab:
+                vocab[word] = len(vocab)
+        else:
+            chars = list(word)
+            for char in chars:
+                if char not in vocab:
+                    vocab[char] = len(vocab)
+    print('processed vocab size ', len(vocab))
+    return vocab
+
+
+def main():
+    # 对原始语料分词  注意答案的分词方式是通过语料中的 | 直接分词的
+    path = os.path.join(train_folder, train_filename)
+    data = seg_data(path)
+
+    # [[question,doc,answer,id],[],[]...]  word_count = {"":}
+    word_count = build_word_count(data)
+
+    with open('data/word-count.pickle', 'wb') as f:
+        pickle.dump(word_count, f)
+
+    vocab = build_vocab(word_count, min_word_freq)  # 对分词之后的建立所有，小于threshold的词将被分成char，然后加入词表
+    with open('data/vocab.pickle', 'wb') as f:
+        pickle.dump(vocab, f)
 
 
 if __name__ == '__main__':
-    analyze()
+    main()
