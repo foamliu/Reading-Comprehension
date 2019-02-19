06b1f9c21ff1f5585ef39cb64c8cadffa70d5adf
unknown <foamliu@yeah.net>
2019-02-19 21:22:37 +0800

    update

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
+import random
+
 import torch
 from torch.autograd import Variable
 from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
 
 from config import batch_size
 from data_gen import AiChallengerDataset
@@ -15,15 +18,34 @@ if __name__ == '__main__':
     dset = AiChallengerDataset()
     dset.set_mode('test')
     test_loader = DataLoader(dset, batch_size=batch_size, shuffle=False, collate_fn=pad_collate)
-    test_acc = 0
-    cnt = 0
 
-    for batch_idx, data in enumerate(test_loader):
+    test_samples = range(len(dset))
+    _ids = random.sample(test_samples, 10)
+    contexts = []
+    questions = []
+    alternatives = []
+
+    for id in _ids:
+        context = dset[id][0]
+        context = [''.join([dset.QA.VOCAB[id] for id in sentence]) for sentence in context]
+        context = '。'.join(context)
+        question = dset[id][1]
+        question = ''.join([dset.QA.IVOCAB[id] for id in question])
+        alternative = dset[id][3]
+        alternative = [dset.QA.IVOCAB[id] for id in alternative]
+        alternative = '|'.join(alternative)
+        print(context)
+        print(question)
+        print(alternative)
+
+    _pred_ids = []
+
+    for i, data in tqdm(enumerate(test_loader)):
         contexts, questions, _, alternatives = data
-        batch_size = contexts.size()[0]
         contexts = Variable(contexts.long().cuda())
         questions = Variable(questions.long().cuda())
         alternatives = Variable(alternatives.long().cuda())
 
         preds = model.forward(contexts, questions, alternatives)
         _, pred_ids = torch.max(preds, dim=1)
+        _pred_ids += list(pred_ids.cpu().numpy())
