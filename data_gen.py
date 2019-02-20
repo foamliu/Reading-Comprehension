e3d7e475626cfea64baad1151ff0f064219d7d62
Yang Liu <liuyang12@focusmedia.cn>
2019-02-20 10:12:47 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -34,10 +34,10 @@ def pad_collate(batch):
         question = np.pad(question, (0, max_question_len - len(question)), 'constant', constant_values=0)
         alternative = np.array(alternative)
         batch[i] = (context, question, answer, alternative)
-        print('context.shape: ' + str(context.shape))
-        print('question.shape: ' + str(question.shape))
-        print('alternative.shape: ' + str(alternative.shape))
-        print('answer: ' + str(answer))
+        # print('context.shape: ' + str(context.shape))
+        # print('question.shape: ' + str(question.shape))
+        # print('alternative.shape: ' + str(alternative.shape))
+        # print('answer: ' + str(answer))
     return default_collate(batch)
 
 
